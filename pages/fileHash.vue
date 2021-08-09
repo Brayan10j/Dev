e63d3ed6b71629a4ed0588edc2472da608db3fc4
Brayan <brayanespitia1@gmail.com>
2021-08-09 15:16:09 -0500

    form filehash

--- pages/fileHash.vue
+++ pages/fileHash.vue
@@ -3,27 +3,74 @@
     <v-overlay :value="overlay">
       <v-progress-circular indeterminate size="150"></v-progress-circular>
     </v-overlay>
-    <v-card class="mx-auto" max-width="800">
+    <v-card class="mx-auto" max-width="600">
       <v-card-title>Hash File</v-card-title>
 
       <v-card-text class="text--primary">
         <v-file-input label="File input" @change="getFile"></v-file-input>
-        Your file has is :
+        Your file has is : 
         <br />
-        <v-btn
-          color="blue"
-          text
-          :href="`https://ipfs.io/ipfs/${hashFile}`"
-          target="_blank"
-        >
-          {{ hashFile }}
-        </v-btn>
+        {{ registroJSON.hashFile }}
+        
       </v-card-text>
 
       <v-card-actions>
-        <v-btn color="blue" text> Register </v-btn>
+        <v-btn v-show="resgister" color="blue" text @click="dialogCreateRegistro = true" > Register </v-btn>
       </v-card-actions>
     </v-card>
+    <v-dialog v-model="dialogCreateRegistro" persistent max-width="900px">
+      <v-overlay :value="overlay">
+        <v-progress-circular indeterminate size="150"></v-progress-circular>
+      </v-overlay>
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Registration</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Name"
+                    v-model="registroJSON.name"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Last Name"
+                    v-model="registroJSON.lastName"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Age"
+                    v-model="registroJSON.age"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Nacionality"
+                    v-model="registroJSON.nacionality"
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialogCreateRegistro = false">
+            Close
+          </v-btn>
+          <v-btn color="blue darken-1" text > Save </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -32,8 +79,17 @@
 export default {
   data() {
     return {
+      dialogCreateRegistro : false,
       hashFile: "",
       overlay: false,
+      resgister: false,
+      registroJSON: {
+        name : "",
+        lastName : "",
+        age : "",
+        nacionality: "",
+        hashFile: ""
+      }
     };
   },
   methods: {
@@ -50,9 +106,10 @@ export default {
             "Content-Type": "multipart/form-data",
           },
         });
-        console.log(res)
-        this.hashFile = res.responsive
+        this.registroJSON.hashFile = res.responsive
+        this.resgister = true
       } catch (error) {
+        this.resgister = false
         console.log(error.message);
       }
     },
