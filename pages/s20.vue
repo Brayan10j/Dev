d6033943e14dab8c3f374030bf05b9054ec9ff56
Brayan <brayanespitia1@gmail.com>
2021-11-25 17:07:49 -0500

    index simulation

--- pages/s20.vue
+++ pages/s20.vue
@@ -1,36 +1,131 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        <v-card width="500">
-          <v-card-title>
-            <span class="text-h5">INDEX FUND</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container>
+  <v-container fluid>
+    <section>
+      <div class="text-h1 mb-6 text-center">{{ marketcap.toFixed(2) }}</div>
+      <div class="text-h5 mb-6 text-center">Estimate ganancies</div>
+    </section>
+    <section>
+      <v-row>
+        <v-col>
+          <v-card width="500">
+            <v-card-title>
+              <span class="text-h5">INDEX FUND</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col>
+                    <v-text-field
+                      v-model.number="value"
+                      label="Quantity "
+                      required
+                      prefix="$"
+                      suffix="(USD)"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions class="mx-auto">
+              <v-spacer></v-spacer>
+              <v-btn color="green" @click="buy"> Deposit </v-btn>
+              <v-btn color="red" @click="withdraw"> Witdraw </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            <v-card-title> TOTAL LOCK VALUE </v-card-title>
+            <v-card-text>
+              <p>
+                <span>{{ marketcap.toFixed(2) }}</span
+                ><span> UST</span>
+              </p>
               <v-row>
                 <v-col>
-                  <v-text-field label="Quantity BNB" required></v-text-field>
+                  <canvas
+                    width="152"
+                    height="152"
+                    style="display: block; box-sizing: border-box"
+                  ></canvas>
+                </v-col>
+                <v-col>
+                  <h3>
+                    <i style="background-color: rgb(75, 219, 75)"></i>
+                    Total Deposit
+                  </h3>
+                  <p>
+                    $
+                    <span>{{ deposit }}</span>
+                  </p>
+                  <h3>
+                    <i style="background-color: rgb(31, 31, 31)"></i>
+                    Total Collateral
+                  </h3>
+                  <p>
+                    $
+                    <span>{{ marketcap.toFixed(2) }}</span>
+                  </p>
                 </v-col>
               </v-row>
-            </v-container>
+              <hr />
+              
+                <v-card-title>TEAM RESERVE</v-card-title>
+                <p>
+                  <span
+                    >$
+                    {{
+                      (tokensTeam * (marketcap / totalSupply)).toFixed(2)
+                    }}</span
+                  ><span> USDT</span>
+                </p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </section>
+    <br />
+    <br />
+
+    <section>
+      <v-card>
+        <v-row>
+          <v-col>
+            <v-card-title> S20 Price</v-card-title>
+            <v-card-text>
+              {{ (marketcap / totalSupply).toFixed(2) }}<span> UST</span>
+            </v-card-text>
+          </v-col>
+          <v-col>
+            <v-card-title> Circulating Supply</v-card-title>
+
+            <v-card-text>{{ totalSupply }}<span> (S20)</span></v-card-text>
+          </v-col>
+          <v-col>
+            <v-card-title> S20 Market Cap</v-card-title>
+            <v-card-text
+              >{{ marketcap.toFixed(2) }}<span> UST</span></v-card-text
+            >
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-card-text>
+            <v-sheet color="rgba(0, 0, 0, .12)">
+              <v-sparkline
+                :value="values"
+                color="rgba(255, 255, 255, .7)"
+                height="100"
+                padding="24"
+                stroke-linecap="round"
+                smooth
+              >
+                <template v-slot:label="item"> ${{ item.value }} </template>
+              </v-sparkline>
+            </v-sheet>
           </v-card-text>
-          <v-card-actions class="mx-auto">
-            <v-spacer></v-spacer>
-            <v-btn color="green"> Buy </v-btn>
-            <v-btn color="red"> Witdraw </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card width="500">
-          <v-card-title>
-            <span class="text-h5">Info</span>
-          </v-card-title>
-          <v-card-text> MarketCap: {{ marketcap }} </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+        </v-row>
+      </v-card>
+    </section>
   </v-container>
 </template>
 
@@ -44,15 +139,78 @@ export default {
   data: () => ({
     to: "",
     marketcap: 0,
+    values: [423, 446, 675, 510, 590, 610, 760],
+    estimate: 0,
+    value: 0,
+    averagePercent: 0,
+    deposit: 0,
+    totalSupply: 0,
+    tokens: 0,
+    tokensTeam: 0,
   }),
   methods: {
     async buy() {
-      var userAccount = web3.currentProvider.selectedAddress;
-      await contract.methods.buyTo().send({ from: userAccount, value: web3.utils.toWei(0, "ether")  });
+      //var userAccount = web3.currentProvider.selectedAddress;
+      //await contract.methods
+      //.buyTo()
+      //.send({ from: userAccount, value: web3.utils.toWei(0, "ether") });
+      this.deposit = this.deposit + this.value;
+      let tokens;
+      if (this.totalSupply == 0) {
+        tokens = this.value;
+        this.marketcap = this.value;
+      } else {
+        tokens = this.value / (this.marketcap / this.totalSupply);
+        this.marketcap = this.marketcap + this.value;
+      }
+      this.tokensTeam = this.tokensTeam + tokens * 0.13;
+      this.totalSupply = this.totalSupply + tokens * 1.13;
+      this.tokens = this.tokens + tokens;
+      this.calculatePercent();
+      this.value = 0;
     },
+    async withdraw() {
+      if (this.value > 0) {
+        if (this.tokens >= this.value) {
+          let withdrawValue = (this.value * this.marketcap) / this.totalSupply;
+          if (this.marketcap >= withdrawValue) {
+            // cambia en el smart contract
+            this.totalSupply = this.totalSupply - this.value;
+            this.tokens = this.tokens - this.value;
+            this.marketcap = this.marketcap - withdrawValue;
+          } else {
+            alert("balance contract insuficient"); // rebundance
+          }
+        } else {
+          alert("tokens insuficient");
+        }
+      } else {
+        alert("value 0");
+      }
+      this.calculatePercent();
+      this.value = 0;
+    },
+    async calculatePercent() {
+      let { data } = await this.$axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=false"
+      );
+      let percentChange = 0;
+      data.forEach((element) => {
+        percentChange = percentChange + element.price_change_percentage_24h;
+      });
+      this.averagePercent = percentChange / data.length / (24 * 60 * 60); // segundos
+    },
+    async calculate() {
+      await this.calculatePercent();
+      setInterval(() => {
+        this.marketcap = this.marketcap * this.averagePercent + this.marketcap;
+        this.estimate = this.marketcap;
+      }, 1000);
+    },
+  },
+  created() {
+    this.calculate();
   },
 };
 </script>
 
-<style>
-</style>
